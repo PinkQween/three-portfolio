a60c50260f11672209b165047b3715462513240a
Hanna <83796834+PinkQween@users.noreply.github.com>
2024-03-07 21:07:51 +0000

    Added SEO

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import { createVuePlugin } from 'vite-plugin-vue2'
+// import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+        react(),
+        // createVuePlugin(),
+        // vueJsx(),
+      ],
 })
